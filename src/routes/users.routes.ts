618d05664bf92f4a9e22a8e8ad9b1e92b64025b9
lprs110 <pro.lucasreis@gmail.com>
2020-06-24 17:22:51 -0400

    feat: create upload context

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // SoC : Separtion of of Concerns (Separação de Preocupação)
 // DTO -> Data Transfer Object
@@ -30,8 +33,26 @@ usersRouter.post('/', async (req, res) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (req, res) => {
-  return res.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+
+      delete user.password;
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
