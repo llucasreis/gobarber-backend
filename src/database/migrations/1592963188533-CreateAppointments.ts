792577ca70011a82d5052f2f3680d101ca511e90
lprs110 <pro.lucasreis@gmail.com>
2020-06-24 10:41:58 -0400

    feat: create users

--- src/database/migrations/1592963188533-CreateAppointments.ts
+++ src/database/migrations/1592963188533-CreateAppointments.ts
@@ -1,7 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1592963188533
-  implements MigrationInterface {
+export default class CreateAppointments1592963188533 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -9,7 +8,7 @@ export default class CreateAppointments1592963188533
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
@@ -17,13 +16,21 @@ export default class CreateAppointments1592963188533
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
         ],
       }),
     );
