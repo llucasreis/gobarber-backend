7b9b1b0a0ea210de3ef82524ccc7878ad87b645e
lprs110 <pro.lucasreis@gmail.com>
2020-06-24 08:24:15 -0400

    chore: add initial project configuration

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+// app.get('/', (req, res) =>  res.json({ message: 'Hello World' }));
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
