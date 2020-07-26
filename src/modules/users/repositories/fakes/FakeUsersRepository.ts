8844d89d12052fb9c6f2ce8400abfbb7f479b8fd
lprs110 <pro.lucasreis@gmail.com>
2020-07-25 22:28:53 -0400

    feat: create tests for users

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,51 @@
+import { uuid } from 'uuidv4';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+
+// SOLID
+
+// Liskov Substitution Principle
+// camadas de banco de dados devem ser possíveis de ser substituídas
+// o service não deve conhecer o repositório
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+    return findUser;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, {
+      id: uuid(),
+      name: userData.name,
+      email: userData.email,
+      password: userData.password,
+    });
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;
