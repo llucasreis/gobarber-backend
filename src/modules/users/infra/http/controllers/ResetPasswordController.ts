cedd655a458aa98a9b9da5fd1805a59df68043b7
lprs110 <pro.lucasreis@gmail.com>
2020-08-03 10:25:58 -0400

    feat: create controllers and migration

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+// index, show, create, update, delete
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { password, token } = req.body;
+
+    const resetPassword = container.resolve(ResetPasswordService);
+
+    await resetPassword.execute({
+      token,
+      password,
+    });
+
+    return res.status(204).json();
+  }
+}
---
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
+
+const passwordRouter = Router();
+const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
+
+// SoC : Separtion of of Concerns (Separação de Preocupação)
+// DTO -> Data Transfer Object
+// Rota: Recebe a requisição, chamar outro arquivo, devolver uma resposta
+
+passwordRouter.post('/forgot', forgotPasswordController.create);
+passwordRouter.post('/reset', resetPasswordController.create);
+
+export default passwordRouter;
+
+// index, show, create, update, delete
