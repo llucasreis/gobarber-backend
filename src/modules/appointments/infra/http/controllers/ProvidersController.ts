a6f0d48f77baab013e1d2d367ea07ff418114d68
lprs110 <pro.lucasreis@gmail.com>
2020-08-03 20:00:14 -0400

    feat: add providers controller/service/repository/test

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProvidersSevice from '@modules/appointments/services/ListProvidersService';
+
+export default class ProvidersController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const user_id = req.user.id;
+
+    const listProviders = container.resolve(ListProvidersSevice);
+
+    const appointment = await listProviders.execute({
+      user_id,
+    });
+
+    return res.json(appointment);
+  }
+}
