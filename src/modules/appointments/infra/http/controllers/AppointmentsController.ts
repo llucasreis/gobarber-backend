c1c2a457dd24127736e0cedb27b586bc7ab9df23
lprs110 <pro.lucasreis@gmail.com>
2020-08-04 11:16:38 -0400

    feat: add structure to create new appointments with new rules

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -5,6 +5,7 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 
 export default class AppointmentsController {
   public async create(req: Request, res: Response): Promise<Response> {
+    const user_id = req.user.id;
     const { provider_id, date } = req.body;
 
     const parsedDate = parseISO(date);
@@ -13,6 +14,7 @@ export default class AppointmentsController {
 
     const appointment = await createAppointment.execute({
       provider_id,
+      user_id,
       date: parsedDate,
     });
 
